cec89a17199fa3b2f6efb2f01e5b910302e94f69
Brian Wawczak <bwawczak@students.mchenry.edu>
2018-09-09 21:15:25 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,20 @@
+public class main {
+
+    public static void main(String[] args) {
+
+
+        String coffeeShop[] = new String[5];
+        coffeeShop[0] = "Coffee cups";
+        coffeeShop[1] = "Sugar packets";
+        coffeeShop[2] = "Plastic Spoons";
+        coffeeShop[3] = "Napkins";
+        coffeeShop[4] = "Milk";
+
+
+        for (int count = 0; count < coffeeShop.length; count++)
+            System.out.println(coffeeShop[count]);
+
+
+    }
+
+}
